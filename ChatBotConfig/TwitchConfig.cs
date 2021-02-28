5439ebacabe8754caadfbb2af738ecf1d65fe52c
Sergey K <i@ksergey.ru>
2021-03-01 01:03:57 +0300

    Добавьте файлы проекта.

---
+++ ChatBotConfig/TwitchConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ChatBotConfig
+{
+    public class TwitchConfig
+    {
+        static TwitchConfig() => config = new TwitchConfig(); 
+
+        public static TwitchConfig config;
+        public static TwitchConfig Instance => config;
+
+
+        string twitchNikname;
+        public string TwitchNikname => twitchNikname;
+       
+        string accessToken;
+        public string AccessToken => accessToken;
+        
+
+        private TwitchConfig()
+        {
+            ///https://twitchtokengenerator.com/
+            this.accessToken = "";
+            this.twitchNikname = ""; // https://www.twitch.tv/twitchNikname
+
+            #region addition
+
+            // Приложения разработчика
+
+            //https://dev.twitch.tv/console/apps
+            //this.idClient = "";
+            //this.botUsername = "";
+
+            #endregion
+        }
+
+
+        #region addition
+
+        //string botUsername;
+        //public string BotUsername => botUsername;
+
+        //string idClient;
+        //public string IdClient => idClient;
+
+        #endregion
+
+    }  
+}
---
+++ TwitchBot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using ChatBotConfig;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+
+namespace TwitchBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            new Bot(TwitchConfig.Instance).Start();
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
